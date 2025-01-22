cf37ebb4c085c03ff2e59365c546906c49c6265d
Rana-Shamroz <mr.shamrozz@gmail.com>
2025-01-22 20:30:14 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+  titleWeb: "Battameez hu ji",
+  introTitle: "battameez the Great",
+  introDesc: `Yrrr Kitni Battameez ho Aap`,
+  btnIntro: "^^Click kro battameez^^",
+  title: "Maanti ho naa k aap battameez ho??",
+  desc: "Sharmaiye matt maan jaiye",
+  btnYes: "haan",
+  btnNo: "nahi",
+  question: "faltu h kuch bhi likh do..",
+  btnReply: "Send",
+  reply: "Ni ho toh koi baat nhi",
+  mess: "I knew it Uh are bataameez",
+  messDesc: "Battameez ki jay ho.",
+  btnAccept: "haan haan thik hai",
+  messLink: "#", // Your messaging link, e.g., https://www.facebook.com/messages/t/100014188333536
+};
